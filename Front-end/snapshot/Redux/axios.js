f09abdb629c8e6bd024ecfa97416f021b8138e9a
Harikrishnanmangeeri <hkmangeeri@gmail.com>
2023-11-25 17:59:42 +0530

    error

--- Front-end/snapshot/Redux/axios.js
+++ Front-end/snapshot/Redux/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:'https://snapshot-998j.onrender.com',
+    baseURL:'https://snapshot-998j.onrender.com/',
     headers:{
         'Content-Type':'application/json'
     },
